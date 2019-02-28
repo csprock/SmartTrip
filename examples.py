c8e9b1527cc93694d4b86e7f8849e87f6e5b4e15
csprock <csprock@gmail.com>
2019-02-27 21:35:44 -0600

    updated demo notebook and examples

--- examples.py
+++ examples.py
@@ -12,3 +12,23 @@ class AuthorsExample:
                TimeWindow(75, 100))
 
     travel_times = (6,2,4,10,10,15)
+
+
+class ExampleA:
+
+    WINDOWS = (TimeWindow(11, 20),
+               TimeWindow(20, 20),
+               TimeWindow(50, 53),
+               TimeWindow(61, 70),
+               TimeWindow(70, 70),
+               TimeWindow(100, 100))
+
+    travel_times = (9, 10, 1, 9, 10)
+
+class ImpossibleExampleA:
+
+    WINDOWS = (TimeWindow(9, 10),
+               TimeWindow(10, 11),
+               TimeWindow(11, 12))
+
+    travel_times = (10, 0.5)
